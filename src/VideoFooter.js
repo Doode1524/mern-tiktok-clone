71f236f0efbe953c00c392f6fab5329849b0ccba
joey <doode1524@gmail.com>
2021-06-19 16:49:10 -0400

    finish video footer

---
+++ src/VideoFooter.js
@@ -0,0 +1,32 @@
+import React from "react";
+import "./VideoFooter.css";
+import MusicNoteIcon from "@material-ui/icons/MusicNote";
+import Ticker from 'react-ticker'
+
+function VideoFooter() {
+  return (
+    <div className="videoFooter">
+      <div className="videoFooter__text">
+        <h3>@joey</h3>
+        <p>This is some sort of description</p>
+        <div className="videoFooter__ticker">
+          <MusicNoteIcon className="videoFooter__icon" />
+          <Ticker mode="smooth">
+              {({  }) => (
+                  <>
+                  <p>Song</p>
+                  </>
+              )}
+          </Ticker>
+        </div>
+      </div>
+      <img
+        className="videoFooter__record"
+        src="https://static.thenounproject.com/png/934821-200.png"
+        alt=""
+        />
+    </div>
+  );
+}
+
+export default VideoFooter;
