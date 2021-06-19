dc17dd546eaccde2ad03f353962516f04a75b65f
joey <doode1524@gmail.com>
2021-06-18 22:48:49 -0400

    init

---
+++ src/Video.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Video.css";
+
+function Video() {
+  return (
+    <div className="video">
+      <video src="https://www.tiktok.com/@adamw/video/6970394216459554054?is_copy_url=0&is_from_webapp=v1&sender_device=pc&sender_web_id=6966666590046733830"></video>
+    </div>
+  );
+}
+
+export default Video;
