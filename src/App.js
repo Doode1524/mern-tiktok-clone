ffa6832eb1fbc55b99c26ceb97648874c07c3144
joey <doode1524@gmail.com>
2021-06-19 15:47:12 -0400

    video component and snap feature

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
-import './App.css';
+import React from "react";
+import "./App.css";
+import Video from "./Video";
 
 function App() {
   return (
     <div className="app">
       <h1>Hello</h1>
+      <div className="app__videos">
+        <Video />
+        <Video />
+      </div>
     </div>
   );
 }
--- src/Video.js
+++ src/Video.js
@@ -4,7 +4,10 @@ import "./Video.css";
 function Video() {
   return (
     <div className="video">
-      <video src="https://www.tiktok.com/@adamw/video/6970394216459554054?is_copy_url=0&is_from_webapp=v1&sender_device=pc&sender_web_id=6966666590046733830"></video>
+      <video
+      className="video__player" 
+      loop
+      src="https://v39-us.tiktokcdn.com/e4436aac7fe06330b94b6f8d7881bfc5/60ce9599/video/tos/useast2a/tos-useast2a-ve-0068c004/363eee73ec9d4a7f8a015ce35c6c835a/?a=1233&br=1516&bt=758&cd=0%7C0%7C1&ch=0&cr=0&cs=0&cv=1&dr=0&ds=3&er=&l=202106191910390101902080132C31BC71&lr=tiktok_m&mime_type=video_mp4&net=0&pl=0&qs=0&rc=MzM1d2pwcm1yNTMzNzczM0ApNTc6NDhmO2U0NzdpaWVkOmdzMHMtYzRoMHBgLS1kMTZzc2MuYF9fLzIwLTY1LjVfYzA6Yw%3D%3D&vl=&vr="></video>
     </div>
   );
 }
