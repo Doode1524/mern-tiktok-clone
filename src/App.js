e642dd9341744112b26eea39c013312fcf4c8878
joey <doode1524@gmail.com>
2021-06-20 15:04:39 -0400

    working urls

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <h1>Hello</h1>
       <div className="app__videos">
         <Video
-          url="https://v39-us.tiktokcdn.com/e4436aac7fe06330b94b6f8d7881bfc5/60ce9599/video/tos/useast2a/tos-useast2a-ve-0068c004/363eee73ec9d4a7f8a015ce35c6c835a/?a=1233&br=1516&bt=758&cd=0%7C0%7C1&ch=0&cr=0&cs=0&cv=1&dr=0&ds=3&er=&l=202106191910390101902080132C31BC71&lr=tiktok_m&mime_type=video_mp4&net=0&pl=0&qs=0&rc=MzM1d2pwcm1yNTMzNzczM0ApNTc6NDhmO2U0NzdpaWVkOmdzMHMtYzRoMHBgLS1kMTZzc2MuYF9fLzIwLTY1LjVfYzA6Yw%3D%3D&vl=&vr="
+          url="https://v19-us.tiktokcdn.com/e7eebc72af08b11a21f425dace4690e8/60cfe550/video/tos/useast2a/tos-useast2a-pve-0068/b595c5ad976540ab8310a1b631aef4b1/?a=1233&br=2112&bt=1056&cd=0%7C0%7C1&ch=0&cr=0&cs=0&cv=1&dr=0&ds=3&er=&l=202106201902570101891940680C1B3157&lr=tiktok_m&mime_type=video_mp4&net=0&pl=0&qs=0&rc=M2hwPHRweDtwNjMzNzczM0ApZjNkZ2dkOzs7NzZpNTtpN2deZ3FvXl5xbC5gLS1kMTZzczA2YzVeMl4vYjIyNC5eYDM6Yw%3D%3D&vl=&vr="
           channel="joey"
           description="Sick ass Tik-Tok clone"
           song="Sir Psycho Sexy"
@@ -17,7 +17,7 @@ function App() {
           shares={789}
         />
         <Video
-          url="https://v39-us.tiktokcdn.com/e4436aac7fe06330b94b6f8d7881bfc5/60ce9599/video/tos/useast2a/tos-useast2a-ve-0068c004/363eee73ec9d4a7f8a015ce35c6c835a/?a=1233&br=1516&bt=758&cd=0%7C0%7C1&ch=0&cr=0&cs=0&cv=1&dr=0&ds=3&er=&l=202106191910390101902080132C31BC71&lr=tiktok_m&mime_type=video_mp4&net=0&pl=0&qs=0&rc=MzM1d2pwcm1yNTMzNzczM0ApNTc6NDhmO2U0NzdpaWVkOmdzMHMtYzRoMHBgLS1kMTZzc2MuYF9fLzIwLTY1LjVfYzA6Yw%3D%3D&vl=&vr="
+          url="https://v19-us.tiktokcdn.com/e8c77b561f375eff4d61129c790157a1/60cfe55e/video/tos/useast2a/tos-useast2a-ve-0068c002/f1bd1861842b4f8c81cb97264562fae6/?a=1233&br=2570&bt=1285&cd=0%7C0%7C1&ch=0&cr=0&cs=0&cv=1&dr=0&ds=3&er=&l=202106201902570101891940680C1B3157&lr=tiktok_m&mime_type=video_mp4&net=0&pl=0&qs=0&rc=amdudnM1anh5NTMzNzczM0ApM2dmM2Y2ZWQ8NzxkNjhkaWcyL2lrazRuXmBgLS1kMTZzczIyX2M2Li0wXzQxYy8wNWI6Yw%3D%3D&vl=&vr="
           channel="joey"
           description="Sick ass Tik-Tok clone"
           song="Sir Psycho Sexy"
