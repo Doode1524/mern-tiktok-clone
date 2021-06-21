260113a68e74689a15ec96ed10427f475a792387
joey <doode1524@gmail.com>
2021-06-21 15:51:46 -0400

    ready to deploy

--- src/App.js
+++ src/App.js
@@ -1,44 +1,39 @@
 import axios from "./axios";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import Video from "./Video";
 
 function App() {
-  const [videos, setVideos] = useState([])
+  const [videos, setVideos] = useState([]);
 
   useEffect(() => {
     async function fetchPosts() {
       const response = await axios.get("/v2/posts");
-      setVideos(response.data)
+      setVideos(response.data);
 
-      return req
+      return response;
     }
 
-    fetchPosts()
+    fetchPosts();
   }, []);
 
   return (
     <div className="app">
       <h1>Hello</h1>
       <div className="app__videos">
-        <Video
-          url="https://v19-us.tiktokcdn.com/e7eebc72af08b11a21f425dace4690e8/60cfe550/video/tos/useast2a/tos-useast2a-pve-0068/b595c5ad976540ab8310a1b631aef4b1/?a=1233&br=2112&bt=1056&cd=0%7C0%7C1&ch=0&cr=0&cs=0&cv=1&dr=0&ds=3&er=&l=202106201902570101891940680C1B3157&lr=tiktok_m&mime_type=video_mp4&net=0&pl=0&qs=0&rc=M2hwPHRweDtwNjMzNzczM0ApZjNkZ2dkOzs7NzZpNTtpN2deZ3FvXl5xbC5gLS1kMTZzczA2YzVeMl4vYjIyNC5eYDM6Yw%3D%3D&vl=&vr="
-          channel="joey"
-          description="Sick ass Tik-Tok clone"
-          song="Sir Psycho Sexy"
-          likes={123}
-          messages={456}
-          shares={789}
-        />
-        <Video
-          url="https://v19-us.tiktokcdn.com/e8c77b561f375eff4d61129c790157a1/60cfe55e/video/tos/useast2a/tos-useast2a-ve-0068c002/f1bd1861842b4f8c81cb97264562fae6/?a=1233&br=2570&bt=1285&cd=0%7C0%7C1&ch=0&cr=0&cs=0&cv=1&dr=0&ds=3&er=&l=202106201902570101891940680C1B3157&lr=tiktok_m&mime_type=video_mp4&net=0&pl=0&qs=0&rc=amdudnM1anh5NTMzNzczM0ApM2dmM2Y2ZWQ8NzxkNjhkaWcyL2lrazRuXmBgLS1kMTZzczIyX2M2Li0wXzQxYy8wNWI6Yw%3D%3D&vl=&vr="
-          channel="joey"
-          description="Sick ass Tik-Tok clone"
-          song="Sir Psycho Sexy"
-          likes={123}
-          messages={456}
-          shares={789}
-        />
+        {videos.map(
+          ({ url, channel, description, song, likes, messages, shares }) => (
+            <Video
+              url={url}
+              channel={channel}
+              description={description}
+              song={song}
+              likes={likes}
+              messages={messages}
+              shares={shares}
+            />
+          )
+        )}
       </div>
     </div>
   );
--- src/axios.js
+++ src/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 const instance = axios.create({
     // baseURL = "http://localhost:8000"
-    baseURL = "https://joey-mern-tiktok-backend.herokuapp.com/"
+    baseURL: "https://joey-mern-tiktok-backend.herokuapp.com/"
 })
 
 export default instance
